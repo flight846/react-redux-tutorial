24cbc1782ace98196ec9c4c6c8ca859a4f1f6c02
Yazid <yazid@decision-science.com>
2018-02-20 18:14:30 +0800

    Refactor to class components

---
+++ src/components/search_bar.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+    // render method
+    render() {
+        return <input/>;
+    }
+}
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyDquXbujYTEacgqWnDyplBe9cEs71z6TcU';
+
 // Create a new component. This component should produce
 // some HTML
 const App = () => {
-    return <div>Hi!</div>;
+    return (
+        <div>
+            <SearchBar/>
+        </div>
+    );
 }
 
 // Take this component's generated HTML and put it
