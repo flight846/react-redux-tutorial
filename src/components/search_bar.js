2f10ad101f4fc669b2e1b14b57f87ad09b493345
yazid <yazid@decision-science.com>
2018-02-20 22:15:28 +0800

    Add input event handler

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -3,7 +3,12 @@ import React, { Component } from 'react';
 class SearchBar extends Component {
     // render method
     render() {
-        return <input/>;
+        return <input onChange = { this.onInputChange }/>;
+    }
+
+    // define another method
+    onInputChange(evt) {
+        console.log(evt.target.value);
     }
 }
 
