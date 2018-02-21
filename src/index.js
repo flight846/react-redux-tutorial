4bdc22d959d451cc1fcb8aec7147ed3a150d5f20
Yazid <yazid@decision-science.com>
2018-02-21 16:43:17 +0800

    Add video list

--- src/index.js
+++ src/index.js
@@ -1,18 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyDquXbujYTEacgqWnDyplBe9cEs71z6TcU';
 
 // Create a new component. This component should produce
 // some HTML
-const App = () => {
-    return (
-        <div>
-            <SearchBar/>
-        </div>
-    );
+class App extends Component {
+    // states
+    constructor(props) {
+        super(props)
+
+        this.state = { videos: [] }
+
+        // default YT needs API_KEY, [options], callback
+        YTSearch({ key: API_KEY, term: 'surfboards' }, (data) => {
+            this.setState({ videos: data });
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+                <VideoList videos={ this.state.videos } />
+            </div>
+        );
+    }
 }
 
 // Take this component's generated HTML and put it
